3542b6973fd7389031b288b3cc9e5ebf6b20b932
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-04-27 12:23:36 +0530

    Adding INstallation script

---
+++ install_tools.sh
@@ -0,0 +1,29 @@
+ask_install() {
+    program_name=$1
+    read -p "Do you wish to install $program_name? " yn
+    case $yn in
+        [Yy]* ) install $program_name;;
+        [Nn]* ) echo "Skipping installation for $program_name";;
+        * ) echo "Please answer yes or no.";;
+    esac
+}
+
+
+install() {
+    program_name=$1
+    case $program_name in
+        git ) sudo apt-get install git;
+        nginx ) sudo apt-get install -y nginx nginx-extras;;
+        java8 ) sudo add-apt-repository ppa:webupd8team/java -y;
+                sudo apt-get update;
+                sudo apt-get install -y oracle-java8-installer;
+                sudo apt-get install -y oracle-java8-set-default;;
+        * ) echo "No installation procedure found for $program_name.";;
+    esac
+}
+
+programs_supported=("git" "nginx", "java8")
+
+for program in ${programs_supported[@]}; do
+    ask_install $program
+done
